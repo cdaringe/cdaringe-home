9840edfdb5a9cbd96d5641599849477c4ce15cd7
cdaringe <cdieringer@mrn.org>
2014-06-27 14:26:44 -0600

    Config updates

--- .dbfuncs.sh
+++ .dbfuncs.sh
@@ -2,7 +2,9 @@
 # REQUIRES THAT YOU HAVE A .secrets file in your ~ dir!
 . ~/.secrets
 
-# update database
+# updates the database
+# @param {string} filename - .xml/.json liquibase file describing changes
+# @param {string} dbname - database name
 udb () 
 {
   USERNAME="ni_developers"
@@ -11,11 +13,9 @@ udb ()
   DBNAME=$2
   PORT="5432"
   FILENAME=$1
-  COMMAND = "liquibase --classpath=/usr/lib/java/lib --username=${USERNAME} --password=${PASSWORD} --url=jdbc:postgresql://${DBHOST}:${PORT}/${DBNAME} --changeLogFile=${FILENAME}  --defaultSchemaName=mrsdba --logLevel=info validate"
 
   echo "=== Update DB Request ==="
   echo "username: $USERNAME"
-  #echo "password: $PASSWORD"
   echo "host: $DBHOST"
   echo "dbname: $DBNAME"
   echo "port: 5432"
@@ -24,6 +24,11 @@ udb ()
   liquibase --classpath=/usr/lib/java/lib --username=${USERNAME} --password=${PASSWORD} --url=jdbc:postgresql://${DBHOST}:${PORT}/${DBNAME} --changeLogFile=${FILENAME} --defaultSchemaName=mrsdba --logLevel=info update
 }
 
+
+
+# rollsback the database
+# @param {string} filename - .xml/.json liquibase file describing changes
+# @param {string} dbname - database name
 rdb () 
 {
   USERNAME="ni_developers"
@@ -35,7 +40,6 @@ rdb ()
 
   echo "=== Rollback DB Request ==="
   echo "username: $USERNAME"
-  #echo "password: $PASSWORD"
   echo "host: $DBHOST"
   echo "dbname: $DBNAME"
   echo "port: 5432"
@@ -43,3 +47,19 @@ rdb ()
   echo "===="
   liquibase --classpath=/usr/lib/java/lib --username=${USERNAME} --password=${PASSWORD} --url=jdbc:postgresql://${DBHOST}:${PORT}/${DBNAME} --changeLogFile=$1 --logLevel=info --defaultSchemaName=mrsdba rollbackCount 99
 }
+
+
+# Performans rdb then udb with args passed.  Note: rdb & udb expect same parameters
+rudb() {
+    `rdb "$@"`
+    `udb "$@"` # pass all args from this function into child func
+}
+
+
+
+# Performans udb then rdb with args passed.  Note: rdb & udb expect same parameters
+urdb() {
+    `udb "$@"`
+    `rdb "$@"` # pass all args from this function into child func
+}
+
