6c1eedc4be5c5fc52ab8e0e771e530c7b109eb83
cdaringe <cdieringer@mrn.org>
2014-07-02 17:40:18 -0600

    Config updates

--- .dbfuncs.sh
+++ .dbfuncs.sh
@@ -14,6 +14,7 @@ udb ()
   PORT="5432"
   FILENAME=$1
 
+  echo
   echo "=== Update DB Request ==="
   echo "username: $USERNAME"
   echo "host: $DBHOST"
@@ -21,6 +22,7 @@ udb ()
   echo "port: 5432"
   echo "file: $FILENAME"
   echo "== = = = =="
+  echo 
   liquibase --classpath=/usr/lib/java/lib --username=${USERNAME} --password=${PASSWORD} --url=jdbc:postgresql://${DBHOST}:${PORT}/${DBNAME} --changeLogFile=${FILENAME} --defaultSchemaName=mrsdba --logLevel=info update
 }
 
@@ -38,21 +40,23 @@ rdb ()
   PORT="5432"
   FILENAME=$1
 
+  echo
   echo "=== Rollback DB Request ==="
   echo "username: $USERNAME"
   echo "host: $DBHOST"
   echo "dbname: $DBNAME"
   echo "port: 5432"
   echo "file: $FILENAME"
-  echo '== = = =  =='
+  echo "== = = = =="
+  echo
   liquibase --classpath=/usr/lib/java/lib --username=${USERNAME} --password=${PASSWORD} --url=jdbc:postgresql://${DBHOST}:${PORT}/${DBNAME} --changeLogFile=$1 --logLevel=info --defaultSchemaName=mrsdba rollbackCount 99
 }
 
 
 # Performans rdb then udb with args passed.  Note: rdb & udb expect same parameters
 rudb() {
-    `rdb "$@"`
-    `udb "$@"` # pass all args from this function into child func
+    rdb "$@"
+    udb "$@" # pass all args from this function into child func
 }
 
 
