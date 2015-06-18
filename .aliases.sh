645b308a6d425aec8d440ce6bcc38ece741c315a
cdaringe <cdieringer@mrn.org>
2015-06-18 15:28:35 -0700

    Config updates

--- .aliases.sh
+++ .aliases.sh
@@ -127,7 +127,8 @@ ballervim () {
     curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim && \
     cd ~/.vim/bundle && \
     rm -rf vim-colors-solarized && git clone git://github.com/altercation/vim-colors-solarized.git && \
-    rm -rf syntastic && git clone https://github.com/scrooloose/syntastic.git
+    rm -rf syntastic && git clone https://github.com/scrooloose/syntastic.git && \
+    rm -rf vdebug && git@github.com:joonty/vdebug.git
 }
 gconfigme() {
     git config --global user.name cdaringe && \
