8535de1c6c04284576f41977fa1a9aad4116a9b6
cdaringe <christopher.dieringer@gmail.com>
2017-05-10 09:03:59 -0700

    Config updates

--- .aliases.sh
+++ .aliases.sh
@@ -19,7 +19,7 @@ alias shasum="sha1sum"
 
 export BACKUP_FOLDER=~/Google\ Drive/backup
 function backupdroplet () {
-  rsync -avz --exclude-from "$BACKUP_FOLDER/exclude.txt"  $DROPLET_JR_IP:/www/ "$BACKUP_FOLDER/www";
+  rsync -avz --exclude-from "$BACKUP_FOLDER/exclude.txt"  root@$DROPLET_JR_IP:/www/ "$BACKUP_FOLDER/www";
 }
 
 # file
